120568f3e91421d794d86ad836fa67c4f26a7395
mahmoudmatar01 <mahmoudmatar49@gmail.com>
2024-02-15 16:58:07 +0200

    refactor HealthChecker.java class

--- src/main/java/com/example/khaier/health/HealthChecker.java
+++ src/main/java/com/example/khaier/health/HealthChecker.java
@@ -11,29 +11,20 @@ public class HealthChecker implements HealthIndicator {
 
     @Override
     public Health health() {
-        long freeMemory=Runtime.getRuntime().freeMemory();
-        long totalMemory= Runtime.getRuntime().totalMemory();
-        double freeMemoryPercent=((double)freeMemory/(double) totalMemory)*100;
-        if (freeMemoryPercent > 25){
-            log.info("free_memory: "+freeMemory + " bytes");
-            log.info("total_memory: "+totalMemory +" bytes");
-            log.info("free_memory_percent: "+freeMemoryPercent+" %");
-            return Health.up()
-                    .withDetail("free_memory ",freeMemory +" bytes")
-                    .withDetail("total_memory ",totalMemory +" bytes")
-                    .withDetail("free_memory_percent ",freeMemoryPercent +" %")
-                    .build();
-        }else {
-            log.info("free_memory: "+freeMemory + " bytes");
-            log.info("total_memory: "+totalMemory +" bytes");
-            log.info("free_memory_percent: "+freeMemoryPercent+" %");
-            return Health.down()
-                    .withDetail("free_memory ",freeMemory +" bytes")
-                    .withDetail("total_memory ",totalMemory +" bytes")
-                    .withDetail("free_memory_percent ",freeMemoryPercent +" %")
-                    .build();
+        long freeMemory = Runtime.getRuntime().freeMemory();
+        long totalMemory = Runtime.getRuntime().totalMemory();
+        double freeMemoryPercent = ((double) freeMemory / totalMemory) * 100;
 
+        log.info("free_memory: {} bytes", freeMemory);
+        log.info("total_memory: {} bytes", totalMemory);
+        log.info("free_memory_percent: {}%", freeMemoryPercent);
 
-        }
+        Health.Builder healthBuilder = (freeMemoryPercent > 30) ? Health.up() : Health.down();
+
+        return healthBuilder
+                .withDetail("free_memory", freeMemory + " bytes")
+                .withDetail("total_memory", totalMemory + " bytes")
+                .withDetail("free_memory_percent", freeMemoryPercent + " %")
+                .build();
     }
 }
