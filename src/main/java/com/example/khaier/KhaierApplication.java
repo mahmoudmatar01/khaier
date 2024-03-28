be405f8ef87ec476afc4a5df2f4024941d0c8325
mahmoudmatar01 <mahmoudmatar49@gmail.com>
2024-03-29 00:17:50 +0200

    add some production properties

--- src/main/java/com/example/khaier/KhaierApplication.java
+++ src/main/java/com/example/khaier/KhaierApplication.java
@@ -1,9 +1,24 @@
 package com.example.khaier;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
 @SpringBootApplication
+@OpenAPIDefinition(servers =
+        {
+                @Server(
+                        url = "khaier-production.up.railway.app",
+                        description = "Remote Project for Historian API"
+                ),
+                @Server(
+                        url = "http://localhost:5920",
+                        description = "Matar Local Project for Historian API"
+                )
+        }
+)
 public class KhaierApplication  {
 
     public static void main(String[] args) {
